8a92d794dbb3c461498c031ee8d48dea549dfe46
aschneids <ash.r.schneider@gmail.com>
2013-10-04 14:01:54 -0700

    changed name of files

---
+++ app/SFAppDelegate.h
@@ -0,0 +1,11 @@
+@class SFAppDelegate;
+
+@interface ParseStarterProjectAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, strong) IBOutlet UIWindow *window;
+
+@property (nonatomic, strong) IBOutlet SFAppDelegate *viewController;
+
+@end
